65dedd20f610ee9aad36e7fc3df25a2b75d26e24
Kestrel Grevatt <kestrelgrevatt@KRG-MacBook-Pro.home>
2024-05-09 07:39:11 -0700

    Small text test.

--- UI.R
+++ UI.R
@@ -1,7 +1,9 @@
 library(shiny)
 library(leaflet)
 
+text1 <- "Map of Blodgett Forest Research Station showing treatment areas, management allocation, and size. Hover over and click polygons for additional information."
+
 navbarPage("Blodgett Forest Research Station", id="main",
            tabPanel("Map", leafletOutput("compartment", height=1000)),
            tabPanel("Data", DT::dataTableOutput("data")),
-           tabPanel("Read Me",includeMarkdown("readme.md")))
+           tabPanel("Read Me",text1))

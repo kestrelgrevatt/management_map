819430c28495c5df80983236cdfdfc8a6e777ffd
Kestrel Grevatt <kestrelgrevatt@KRG-MacBook-Pro.home>
2024-05-09 07:50:28 -0700

    Update text.

--- UI.R
+++ UI.R
@@ -2,9 +2,9 @@ library(shiny)
 library(leaflet)
 
 text1 <- "Map of Blodgett Forest Research Station showing treatment areas across forest and their management allocation and size. 
-          Hover over and click polygons for additional information. <br>
+          Hover over and click polygons for additional information.
           Blodgett Forest has been divided into stand-level compartments shown here to 
-          demonstrate and research a broad range of management alternatives across the forest. <br>
+          demonstrate and research a broad range of management alternatives across the forest.
           As the data shown in the Data tab is cleaned and updated, additional treatment detail will be added to map."
 
 

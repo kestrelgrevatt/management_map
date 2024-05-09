f38c19335d6ce6db8986d46cfb2e234c94d7aff2
Kestrel Grevatt <kestrelgrevatt@KRG-MacBook-Pro.home>
2024-05-09 07:46:56 -0700

    additional text

--- UI.R
+++ UI.R
@@ -1,7 +1,12 @@
 library(shiny)
 library(leaflet)
 
-text1 <- "Map of Blodgett Forest Research Station showing treatment areas, management allocation, and size. Hover over and click polygons for additional information."
+text1 <- "Map of Blodgett Forest Research Station showing treatment areas across forest and their management allocation and size. 
+          Hover over and click polygons for additional information. <br>
+          Blodgett Forest has been divided into stand-level compartments shown here to 
+          demonstrate and research a broad range of management alternatives across the forest. <br>
+          As the data shown in the Data tab is cleaned and updated, additional treatment detail will be added to map."
+
 
 navbarPage("Blodgett Forest Research Station", id="main",
            tabPanel("Map", leafletOutput("compartment", height=1000)),
